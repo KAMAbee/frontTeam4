fbef4795a71b5772c28a5811b21344e8574b4829
KAMAbee <kamil.akbarov.94@gmail.com>
2026-02-28 17:34:26 +0500

    basic setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import styles from './index.module.scss'
+import App from './app/App'
+import './styles/global.scss'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <div className={styles.page}>
-      <App />
-    </div>
+    <App />
   </StrictMode>,
 )
